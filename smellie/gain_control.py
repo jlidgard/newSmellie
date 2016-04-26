1fef4c010ed40440e37a7194be0c5c4c9065fa93
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-04-26 18:11:49 +0100

    gain_control clean up

--- smellie/gain_control.py
+++ smellie/gain_control.py
@@ -1,4 +1,5 @@
 import daqmx.fuctions
+import daqmx.constants
 from config import GAIN_CONTROL_N_SAMPLES, GAIN_CONTROL_SAMP_FREQ
 class GainContolLogicError(Exception):
     pass
@@ -11,39 +12,49 @@ class GainVoltageGenerator(object):
 
         self.sampling_frequency = GAIN_CONTROL_SAMP_FREQ
         self.taskHandle = taskHandle
-        aoDetails = str(devName + analogueOutputPin)
-        #not in use below\ for testing purposes. Forced with "Dev1/ao1" argument.
+        #not in use below for testing purposes. Forced with "Dev1/ao1" argument.
         vMin = 0.0
         vMax = 1.0
         vRes = 0.0044 # Residual voltage on MPU
-        if(vGain > 1.0 or vGain < 0.0124):
+        functions.DAQmxCreateAOVoltageChan(self.taskHandle, "Dev1/ao0", "",
+                                           vMin, vMax, 
+                                           constants.DAQmx_Val_Volts,
+                                           None)
+        DAQmxCfgSampClkTiming(self.taskHandle, "", 
+                              self.sampling_frequency,
+                              constants.DAQmx_Val_Rising,
+                              constants.DAQmx_Val_ContSamps,
+                              self.number_of_samples)
+
+    def set_voltage(self, voltage):
+        # FIXME: 0.1 or 0.0124????
+        if(voltage > 1.0 or voltage < 0.0124):
             raise  GainControlLogicError("Gain voltage must be set between 0.1V and 1.0V to avoid damage to MPU")
 
-        DAQmxCreateAOVoltageChan(taskHandle,"Dev1/ao0","",vMin,vMax,D\
-AQmx_Val_Volts,None)
-        DAQmxCfgSampClkTiming(taskHandle,"",sampling_frequency,DAQmx_\
-Val_Rising,DAQmx_Val_ContSamps,self.number_of_samples)
-
-    def set_voltage(self):
         #Creating and shaping the data buffer:
         self.data = np.zeros(3000, dtype = numpy.float64)
         data_length = self.data.shape[0]
 
         for i in range(0, data_length):
             self.data[i]= vGain - vRes
-        DAQmxWriteAnalogF64(self.taskHandle,3000,0,10.0,DAQmx_Val_Gro\
-upByChannel,data,None,None)
+            functions.DAQmxWriteAnalogF64(self.taskHandle, 
+                                          3000, 0, 10.0,
+                                          constants,
+                                          DAQmx_Val_GroupByChannel, 
+                                          data, None, None)
 
         DAQmxStartTask(self.taskHandle)
 
     def __exit__(self):
-        DAQmxStopTask(self.taskHandle)
+        functions.DAQmxStopTask(self.taskHandle)
         data = np.zeros(3000, dtype = numpy.float64)
         data_length = data.shape[0]
         for i in range(0, data_length):
             data[i]= 0.0
-        DAQmxWriteAnalogF64(self.taskHandle,3000,0,10.0,DAQmx_Val_Gro\
-                                upByChannel,data,None,None)
+        functions.DAQmxWriteAnalogF64(self.taskHandle,
+                                      3000, 0, 10.0,
+                                      DAQmx_Val_GroupByChannel, 
+                                      data, None, None)
 
-        DAQmxClearTask(self.taskHandle)
+        functions.DAQmxClearTask(self.taskHandle)
 
