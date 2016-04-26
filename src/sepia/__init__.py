1d49fc278351b405d24cb772a2a45df5c35be60e
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-04-26 17:02:43 +0100

    remove duplicated src folder

--- src/sepia/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-This package gives a python API to a subset of the Sepia provided C-software documented HERE. 
-Not all of the functions are exposed, and there has been one critical modification:
-`sepia.slm.set_pulse_parameters` has the bPulseMode paramter hard coded to 1:
-using bPulseMode = 0 means continuous laser light that could damage the laser
-head. You don't want to change that
-'''
