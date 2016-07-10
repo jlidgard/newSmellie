22796dbacc41bb92d04e4956da8b6bc67294888e
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-07-10 13:38:34 +0100

    remove redundant env

--- env.sh
+++
@@ -1,3 +0,0 @@
-THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-export PYTHONPATH=$THISDIR:$THISDIR/src:$THISDIR/src/server:$THISDIR/src/sepia:$THISDIR/src/server:interlock:$PYTHONPATH
