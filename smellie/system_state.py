c6b5be12b9f721b87b6a3fc4f9a49f203ffec485
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-07-10 23:00:09 +0100

    sys state

--- smellie/system_state.py
+++ smellie/system_state.py
@@ -9,11 +9,14 @@ def get_SHA():
     return check_output(["git", "describe", "--always", "--tag"]).strip()
 
 def git_is_dirty():
-    return check_output(["git", "status", "--porcelain"])
+    '''
+    
+    '''
+    return True if check_output(["git", "status", "--porcelain"]) else False
 
 def get_config_str():
-    """
+    '''
     Reads the config.py module (located one folder-level up from this file) into a string
-    """
+    '''
     return "\n".join("{0} : {1}".format(k, v) for (k, v) in config.__dict__.iteritems() if not k.startswith("__"))
 
