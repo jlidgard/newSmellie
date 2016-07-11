b8df80874627dc80d69c5f60136909e1de08f286
Jack Dunger <jack.dunger@physics.ox.ac.uk>
2016-07-11 20:28:59 +0100

    doc string serer module

--- server/__init__.py
+++ server/__init__.py
@@ -1,3 +1,3 @@
 """
-Code related to the XML-RPC server that runs on SNODROP
+Code related to the XML-RPC server that runs on SNODROP. Each object method exposed is wrapped to give it a "dummy mode and to convert any exceptions thrown to strings to return over the server
 """
